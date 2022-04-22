169c6d769a30ad8f6db7a22e0752ca3b62906d8a
ericstretton <eric.stretton@gmail.com>
2022-04-21 19:17:46 -0600

    Before Practice with vuetify.

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <AxiosContainer/>
+  </div>
+</template>
+
+<script>
+import AxiosContainer from '@/components/AxiosContainer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AxiosContainer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+html{
+  background-color: rgb(240, 200, 148);
+}
+</style>
